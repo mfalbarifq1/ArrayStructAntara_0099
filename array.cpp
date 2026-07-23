9924d390b7780adb3108ac30df9298d7a830ca77
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-07-23 14:15:57 +0700

    cara akses

--- array.cpp
+++ array.cpp
@@ -8,3 +8,6 @@ int main(){
 
     //deklarasi tanpa inisialisasi
     int datamu[5];
+    //cara akses(membaca atau mengambil isi array)
+    cout<<"isi data kedua : "<<dataku[1]<<endl;
+    //mengisi array
